26e8809f84000c9fa0b9959c862a8ba32e14c023
Aidan Bird <aidan@birdwebwork.com>
2022-05-14 18:50:35 -0400

    new file uploading form and new dependency (flask-wtf)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 from distutils.log import debug
 from flask import Flask, render_template
@@ -16,6 +17,13 @@ globals.ASSEMBLYAI_API_KEY = os.environ.get('ASSEMBLYAI_API_KEY')
 
 def home_page():
     return render_template('base.html')
+
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
