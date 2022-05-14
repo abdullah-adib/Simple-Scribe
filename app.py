f80b8202cd54f9c2d5c0ab04d96bde24d86a174f
A5TA <cardillojamesbrandon@gmail.com>
2022-05-13 23:16:49 -0400

    Added templates with base.html and modified home_page on app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+
+def home_page():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
